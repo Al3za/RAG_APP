87f22d77b6e01bb5a1416f1ef1cf156622f18a5e
sandro <akecakabro@gmail.com>
2026-03-04 18:18:49 +0100

    add head get

--- app/api/health.py
+++ app/api/health.py
@@ -9,3 +9,9 @@ def healt_check():
         "service":"rag-api" 
     }
 
+# HEAD per i monitor (UptimeRobot)
+@router.head("/healt_head")
+def health_head(): ## per essere chiamato da uptime robot per evitare cold start
+    #  (get puo non funzionar equando viene chiamato da robot)
+    print('hit here')
+    return {}
--- app/api/uptime_robot_healt.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-# HEAD per i monitor (UptimeRobot)
-@router.head("/healt_head")
-def health_head(): ## per essere chiamato da uptime robot per evitare cold start
-    #  (get puo non funzionar equando viene chiamato da robot)
-    print('hit here')
-    return {}
