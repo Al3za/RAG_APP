9c525f75758554459e79e831204e8d3670c0b538
sandro <akecakabro@gmail.com>
2026-03-05 15:05:48 +0100

    add @router.head(/healt) for uptimeRobot ping to avoid cold start for backend on render (mvp) only

--- app/api/health.py
+++ app/api/health.py
@@ -10,7 +10,7 @@ def healt_check():
     }
 
 # HEAD per i monitor (UptimeRobot)
-@router.head("/healt_head")
+@router.head("/healt")
 def health_head(): ## per essere chiamato da uptime robot per evitare cold start
     #  (get puo non funzionar equando viene chiamato da robot)
     print('hit here')
